e56e6563702e7de484aefb78469093e0274f11d3
PeterRender <webpismo@yahoo.de>
2026-03-30 22:33:39 +0000

    Реализация ThreadPool
    - std::jthread (C++20) для автоматического управления жизнью потоков
    - Атомарный флаг stop_ с memory_order_release/acquire
    - Конструктор с числом потоков по умолчанию = hardware_concurrency()
    - Защита от 0-го количества потоков (min 1)
    - worker() обрабатывает завершение работы через pop() == nullopt
    - Перехват исключений из задач
    
    Проект компилируется без ошибок

--- include/thread_pool/thread_pool.hpp
+++ include/thread_pool/thread_pool.hpp
@@ -1,9 +1,38 @@
 #pragma once
 
+#include "queue/priority_queue.hpp"  // приоритетная очередь задач
+
+#include <atomic>  // подключение стандартного шаблона атомарной переменной
+#include <memory>  // подключение стандартного шаблона разделяемого невладеющего указателя
+#include <thread>  // подключение стандартного RAII-потока (std::jthread)
+#include <vector>  // подключение стандартного шаблона динамического массива
+
 namespace dispatcher::thread_pool {
 
+// Класс пула потоков-воркеров
 class ThreadPool {
-  // здесь ваш код
+public:
+    // Явный параметрический конструктор, инициализирующий пул потоков
+    // Принимает общую очередь задач и число потоков (по умолчанию - число потоков с аппаратным параллелизмом)
+    explicit ThreadPool(std::shared_ptr<queue::PriorityQueue> task_queue,
+                        size_t num_threads = std::thread::hardware_concurrency());
+
+    // Деструктор (сигнализирует о завершении работы и дожидается выполнения всех потоков)
+    ~ThreadPool();
+
+    // Запрещаем копирование и перемещение
+    ThreadPool(const ThreadPool &) = delete;
+    ThreadPool &operator=(const ThreadPool &) = delete;
+    ThreadPool(ThreadPool &&) = delete;
+    ThreadPool &operator=(ThreadPool &&) = delete;
+
+private:
+    // Рабочий метод потока-воркера (постоянно извлекает задачи из очереди и выполняет их)
+    void worker();
+
+    std::shared_ptr<queue::PriorityQueue> task_queue_;  // общая очередь задач с приоритетами
+    std::vector<std::jthread> workers_;                 // массив потоков-воркеров
+    std::atomic<bool> stop_{false};                     // атомарный флаг прекращения работы
 };
 
-} // namespace dispatcher::thread_pool
+}  // namespace dispatcher::thread_pool
--- src/thread_pool/thread_pool.cpp
+++ src/thread_pool/thread_pool.cpp
@@ -1,7 +1,54 @@
-#include "thread_pool/thread_pool.hpp"
+#include "thread_pool/thread_pool.hpp"  // интерфейс класса пула потоков-воркеров
+
+#include <algorithm>  // подключение std::max
 
 namespace dispatcher::thread_pool {
 
-// здесь ваш код
+// Параметрический конструктор, инициализирующий пул потоков
+ThreadPool::ThreadPool(std::shared_ptr<queue::PriorityQueue> task_queue, size_t num_threads)
+    : task_queue_(std::move(task_queue)) {
+    // Резервируем память под массив потоков-воркеров
+    // (hardware_concurrency() может вернуть 0, если информация недоступна - ставим защиту)
+    workers_.reserve(std::max<size_t>(num_threads, 1));
 
-} // namespace dispatcher::thread_pool
+    // Заполняем массив потоков-воркеров
+    for (size_t i = 0; i < num_threads; ++i) {
+        workers_.emplace_back(&ThreadPool::worker, this);  // запускаем метод worker в отдельном потоке
+    }
+}
+
+// Деструктор (сигнализирует о завершении работы и дожидается выполнения всех потоков)
+ThreadPool::~ThreadPool() {
+    stop_.store(true, std::memory_order_release);  // поднимаем флаг прекращения работы
+
+    // Сигнализируем очереди о завершении работы (будим все заблокированные pop())
+    if (task_queue_) {
+        task_queue_->shutdown();
+    }
+    // Примечание: при разрушении массива потоков-воркеров объекты std::jthread
+    // автоматически дожидаются завершения работы своих методов worker
+}
+
+// Рабочий метод потока-воркера (постоянно извлекает задачи из очереди и выполняет их)
+void ThreadPool::worker() {
+    // Работаем, пока не получим сигнал завершения
+    while (!stop_.load(std::memory_order_acquire)) {
+        // Пытаемся извлечь задачу из очереди (блокируется, если задач нет)
+        auto task = task_queue_->pop();
+
+        // Если задача не извлечена (получен stop_ и очередь пуста) - выходим из цикла
+        if (!task) {
+            break;
+        }
+
+        // Задача есть - пытаемся выполнить ее
+        try {
+            (*task)();  // вызываем функцию задачи
+        } catch (...) {
+            // Ловим все исключения, чтобы поток не упал (задача не должна нарушить работу пула)
+            // Здесь можно логировать ошибку
+        }
+    }
+}
+
+}  // namespace dispatcher::thread_pool
